0e80da31f8575b8d52c4aa3a6be852c8ab5ee19c
Andri4nina <138116935+Andri4nina@users.noreply.github.com>
2025-03-26 10:05:56 +0300

    maj purchaseRequest

--- src/purchase-request/purchase-request.controller.ts
+++ src/purchase-request/purchase-request.controller.ts
@@ -1,8 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { PurchaseRequestService } from './purchase-request.service';
 
 @Controller('api/purchase-request')
 export class PurchaseRequestController {
+    constructor(private readonly purchaseRequestService: PurchaseRequestService) { }
 
+    @Post()
+    create(@Body() data: any) {
+        return this.purchaseRequestService.create(data);
+    }
 
-    
+    @Get()
+    findAll() {
+        return this.purchaseRequestService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.purchaseRequestService.findOne(+id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() data: any) {
+        return this.purchaseRequestService.update(+id, data);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.purchaseRequestService.remove(+id);
+    }
+
+    @Post(':id/validate/:validatorId')
+    async validate(
+        @Param('id') id: string,
+        @Param('validatorId') validatorId: string,
+    ) {
+        return this.purchaseRequestService.validatePurchaseRequest(
+            parseInt(id),
+            parseInt(validatorId),
+        );
+    }
+
 }
