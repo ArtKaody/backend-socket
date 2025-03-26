ba87a5035531177435aa3d6dac11de5f4e24c6c6
Andri4nina <138116935+Andri4nina@users.noreply.github.com>
2025-03-26 20:48:05 +0300

    mise a jour bd

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import * as cors from 'cors';
+
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  app.use(
+    cors({
+    origin: 'http://localhost:5173', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+    
+  }));  
+  
+  
+  
+
+
+  
+  await app.listen(3000);
 }
 bootstrap();
